5919382c1c64de25a372e9690020f83beea0bb38
abhina-s <abhina@thetripguru.com>
2020-08-11 01:04:07 +0530

    made strict boundaries for hero

--- js/game.js
+++ js/game.js
@@ -1,5 +1,4 @@
 // Create the canvas
-
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 512;
@@ -64,16 +63,24 @@ var reset = function () {
 // Update game objects
 var update = function (modifier) {
     if (38 in keysDown) { // Player holding up
-        hero.y -= hero.speed * modifier;
+        if (hero.y > 2) {
+            hero.y -= hero.speed * modifier;
+        }
     }
     if (40 in keysDown) { // Player holding down
-        hero.y += hero.speed * modifier;
+        if (hero.y < 440) {
+            hero.y += hero.speed * modifier;
+        }
     }
     if (37 in keysDown) { // Player holding left
-        hero.x -= hero.speed * modifier;
+        if (hero.x > 0) {
+            hero.x -= hero.speed * modifier;
+        }
     }
     if (39 in keysDown) { // Player holding right
-        hero.x += hero.speed * modifier;
+        if (hero.x < 460) {
+            hero.x += hero.speed * modifier;
+        }
     }
 
     // Are they touching?
